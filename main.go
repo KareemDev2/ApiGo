8f56f527e1816d936641b8ec78b5adcd308dc78d
KareemDev2 <karim.ounasser.contact@gmail.com>
2024-11-06 14:44:25 +0000

    Déclaration et initialisation

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
-// Player représente un joueur avec les attributs nom, pseudo, âge, santé et mana.
+import "fmt"
+
+// Déclaration de la structure Player
 type Player struct {
 	Nom    string
 	Pseudo string
 	Age    int
 	Health int
 	Mana   int
+}
+
+func main() {
+	// Déclaration et initialisation de la map "players"
+	players := map[string]Player{
+		"Dupont": {Nom: "Tommy", Pseudo: "Tom", Age: 25, Health: 100, Mana: 50},
+		"Martin": {Nom: "Jerry", Pseudo: "Jee", Age: 30, Health: 80, Mana: 60},
+	}
+
+	// Affichage des informations des joueurs
+	for nom, player := range players {
+		fmt.Printf("Nom: %s, Player: %+v\n", nom, player)
+	}
 }
