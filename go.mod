8cb69bd14d19b8ad3e038a1144d89fef82f4c4ca
KareemDev2 <karim.ounasser.contact@gmail.com>
2024-11-06 15:00:14 +0000

    fonction save avec fichier yaml

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example.com
 
 go 1.22.7
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
--- main.go
+++ main.go
@@ -1,25 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
 
 // Déclaration de la structure Player
 type Player struct {
-	Nom    string
-	Pseudo string
-	Age    int
-	Health int
-	Mana   int
+	Nom    string `yaml:"nom"`
+	Pseudo string `yaml:"pseudo"`
+	Age    int    `yaml:"age"`
+	Health int    `yaml:"health"`
+	Mana   int    `yaml:"mana"`
+}
+
+// Méthode save pour sauvegarder les données du joueur dans un fichier YAML
+func (p *Player) save() error {
+	// Vérifie si le nom est vide
+	if p.Nom == "" {
+		return fmt.Errorf("le nom du joueur ne peut pas être vide")
+	}
+
+	// Conversion de la structure Player en YAML
+	data, err := yaml.Marshal(p)
+	if err != nil {
+		return fmt.Errorf("erreur lors de la conversion en YAML : %v", err)
+	}
+
+	// Nom du fichier basé sur le champ Nom du joueur
+	filename := p.Nom + ".yml"
+
+	// Écriture des données YAML dans le fichier
+	err = os.WriteFile(filename, data, 0644)
+	if err != nil {
+		return fmt.Errorf("erreur lors de l'écriture dans le fichier : %v", err)
+	}
+
+	return nil
 }
 
 func main() {
-	// Déclaration et initialisation de la map "players"
-	players := map[string]Player{
-		"Dupont": {Nom: "Tommy", Pseudo: "Tom", Age: 25, Health: 100, Mana: 50},
-		"Martin": {Nom: "Jerry", Pseudo: "Jee", Age: 30, Health: 80, Mana: 60},
+	// Création d'un exemple de joueur
+	player := Player{
+		Nom:    "Dupont",
+		Pseudo: "Dupu",
+		Age:    25,
+		Health: 100,
+		Mana:   50,
 	}
 
-	// Affichage des informations des joueurs
-	for nom, player := range players {
-		fmt.Printf("Nom: %s, Player: %+v\n", nom, player)
+	// Sauvegarde des données du joueur dans un fichier
+	if err := player.save(); err != nil {
+		log.Fatalf("Erreur de sauvegarde : %v", err)
+	} else {
+		fmt.Println("Données du joueur sauvegardées avec succès.")
 	}
 }
